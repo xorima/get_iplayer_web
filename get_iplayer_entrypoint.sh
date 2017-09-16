d31774fefa7b8a1503f96de2cb044de0f84d5826
Jason Field <jason@avon-lea.co.uk>
2017-09-16 21:58:51 +0100

    Updated uid and gid

---
+++ get_iplayer_entrypoint.sh
@@ -0,0 +1,1 @@
+get_iplayer --output '/data/output' --outputtv='/data/output/tv/' --outputradio='/data/output/radio' --fileprefix '<nameshort><.senum>' --subdir --subdir-format='<nameshort>/Series <seriesnum>'
